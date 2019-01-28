9323b71398e26dfe4bd5ef47be155781dd1ba22c
dntran7 <dntran7@asu.edu>
2019-01-27 22:26:48 -0700

    fixed bug where items can only start in one location, added random coors

--- app/src/main/java/com/example/shiva/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/shiva/splashscreen/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.ClipData;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -25,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
     private int _xDelta;
     private int _yDelta;
     private int count;
+
+    static int relativeposimage1top=0;
+    static int relativeposimage1bot=0;
+    static int relativeposimage2top=0;
+    static int relativeposimage2bot=0;
+    static int relativeposimage3top=0;
+    static int relativeposimage3bot=0;
+    int dimensionswidth=700;     ///arbitrary value because view.getWidth() sCREwed Up mY pHONe Real bAD gUYS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,24 +42,34 @@ public class MainActivity extends AppCompatActivity {
         image = findViewById(R.id.imageView);
         image2 = findViewById(R.id.imageView2);
         image3 = findViewById(R.id.imageView3);
-        target2 = findViewById(R.id.trash);
         debugchannel = findViewById(R.id.debug);
+
+        target2 = findViewById(R.id.trash);
         target = findViewById(R.id.actualtrash);
+        //RelativeLayout.LayoutParams layoutParamstrash = new RelativeLayout.LayoutParams(150, 150);
+        //target2.setLayoutParams(layoutParamstrash);
+        //RelativeLayout.LayoutParams layoutParamsactualtrash = new RelativeLayout.LayoutParams(150, 150);
+        //target.setLayoutParams(layoutParamsactualtrash);
+
         points = findViewById(R.id.pts);
         score = 0;
         count = 0;
         points.setText("" + score);
+
         image.setOnTouchListener(new ChoiceTouchListener());
         image2.setOnTouchListener(new ChoiceTouchListener());
         image3.setOnTouchListener(new ChoiceTouchListener());
         //image2.setOnLongClickListener(longClickListener);
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
+
+        layoutParams.setMargins((int)(Math.random()*dimensionswidth),150,300,500);
         //layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
         image.setLayoutParams(layoutParams);
         RelativeLayout.LayoutParams layoutParams1 = new RelativeLayout.LayoutParams(150, 150);
-        //layoutParams1.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        layoutParams1.setMargins((int)(Math.random()*dimensionswidth),150,300,500);
         image2.setLayoutParams(layoutParams1);
         RelativeLayout.LayoutParams layoutParams2 = new RelativeLayout.LayoutParams(150, 150);
+        layoutParams2.setMargins((int)(Math.random()*dimensionswidth),150,300,500);
         //layoutParams2.addRule(RelativeLayout.CENTER_HORIZONTAL);
         image3.setLayoutParams(layoutParams2);
         moveAnimation(image, 500f);
@@ -76,13 +96,25 @@ public class MainActivity extends AppCompatActivity {
                     RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
                     _xDelta = X - lParams.leftMargin;
                     _yDelta = Y - lParams.topMargin;
-                    animation.cancel();
+                    if(animation.isRunning())
+                    {
+                        relativeposimage2bot = 0-(int)image2.getY()-150;
+                        relativeposimage2top = 0-(int)image2.getY()-300;
+
+                        relativeposimage1bot = 0-(int)image.getY()-150;
+                        relativeposimage1top = 0-(int)image.getY()-300;
+
+                        relativeposimage3bot = 0-(int)image3.getY()-150;
+                        relativeposimage3top = 0-(int)image3.getY()-300;
+                        animation.cancel();
+                    }
+
                     break;
                 case MotionEvent.ACTION_UP:
                     int targetRight = target.getRight();
                     int targetLeft = target.getLeft();
                     int targetTop = target.getTop();
-                    int targetBottom = target.getBottom();
+                    int targetBottom = target.getBottom()+150;
 
                     int targetRight2 = target2.getRight();
                     int targetLeft2 = target2.getLeft();
@@ -90,22 +122,22 @@ public class MainActivity extends AppCompatActivity {
                     int targetBottom2 = target2.getBottom();
 
                     if (view.getId() == R.id.imageView) {
-                        int imgRight = image.getRight() ;
-                        int imgLeft = image.getLeft();
-                        int imgTop = image.getTop();
-                        int imgBottom = image.getBottom();
+                        int imgRight = image.getRight()-70;
+                        int imgLeft = image.getLeft()+70;
+                        int imgTop = image.getTop()-relativeposimage1top+70;
+                        int imgBottom = image.getBottom()-relativeposimage1bot-70;
 
                 /*debugchannel.setText("target right: " + targetRight + "; target left: " + targetLeft + "; img right: " + imgRight + "; img left: " + imgLeft
                                 + "target top: " + targetTop + "; target bottom: " + targetBottom + "; img top: " + imgTop + "; img bottom: " + imgBottom);
 */
-                        if (targetRight > imgRight && targetLeft < imgLeft && targetTop > imgTop
+                        if (targetRight > imgRight && targetLeft < imgLeft && targetTop < imgTop
                                 && targetBottom > imgBottom && image.getTag().equals("trash")) {
                             image.setVisibility(View.GONE);
                             score++;
                             points.setText("" + score);
                             count++;
                         }
-                        else if(targetRight2 > imgRight && targetLeft2 < imgLeft && targetTop2 > imgTop
+                        else if(targetRight2 > imgRight && targetLeft2 < imgLeft && targetTop2 < imgTop
                                 && targetBottom2 > imgBottom && image.getTag().equals("trash"))
                         {
                             hitCheck();
@@ -113,19 +145,21 @@ public class MainActivity extends AppCompatActivity {
                     }
                     else if(view.getId()==R.id.imageView2)
                     {
-                        int imgRight = image2.getRight();
-                        int imgLeft = image2.getLeft();
-                        int imgTop = image2.getTop();
-                        int imgBottom = image2.getBottom();
+                        int imgRight = image2.getRight()-70;
+                        int imgLeft = image2.getLeft()+70;
+                        int imgTop = image2.getTop()-relativeposimage2top+70;
+                        int imgBottom = image2.getBottom()-relativeposimage2bot-70;
 
-                       /* debugchannel.setText("target right: " + targetRight + "; target left: " + targetLeft + "; img right: " + imgRight + "; img left: " + imgLeft
+                       debugchannel.setText("target right: " + targetRight + "; target left: " + targetLeft + "; img right: " + imgRight + "; img left: " + imgLeft
                                 + "target top: " + targetTop + "; target bottom: " + targetBottom + "; img top: " + imgTop + "; img bottom: " + imgBottom);
-*/
-                        if (targetRight > imgRight && targetLeft < imgLeft && targetTop > imgTop
+
+                        if (targetRight > imgRight && targetLeft < imgLeft && targetTop < imgTop
                                 && targetBottom > imgBottom && image2.getTag().equals("recycle")) {
+                            debugchannel.setText("lmao"+"target right: " + targetRight + "; target left: " + targetLeft + "; img right: " + imgRight + "; img left: " + imgLeft
+                                    + "target top: " + targetTop + "; target bottom: " + targetBottom + "; img top: " + imgTop + "; img bottom: " + imgBottom);
                             hitCheck();
                         }
-                        else if(targetRight2 > imgRight && targetLeft2 < imgLeft && targetTop2 > imgTop
+                        else if(targetRight2 > imgRight && targetLeft2 < imgLeft && targetTop2 < imgTop
                                 && targetBottom2 > imgBottom && image2.getTag().equals("recycle"))
                         {
                             image2.setVisibility(View.GONE);
@@ -137,12 +171,14 @@ public class MainActivity extends AppCompatActivity {
 
                     else if(view.getId()==R.id.imageView3)
                     {
-                        int imgRight = image3.getRight();
-                        int imgLeft = image3.getLeft();
-                        int imgTop = image3.getTop();
-                        int imgBottom = image3.getBottom();
+                        int imgRight = image3.getRight()-30;
+                        int imgLeft = image3.getLeft()+30;
+                        int imgTop = image3.getTop()-relativeposimage3top+100;
+                        int imgBottom = image3.getBottom()-relativeposimage3bot-150;
 
-                        if (targetRight > imgRight && targetLeft < imgLeft && targetTop > imgTop
+                        //debugchannel.setText("target right: " + targetRight2 + "; target left: " + targetLeft2 + "; img right: " + imgRight + "; img left: " + imgLeft
+                             //   + "target top: " + targetTop2 + "; target bottom: " + targetBottom2 + "; img top: " + imgTop + "; img bottom: " + imgBottom);
+                        if (targetRight > imgRight && targetLeft < imgLeft && targetTop < imgTop
                                 && targetBottom > imgBottom && image3.getTag().equals("trash")) {
 
 
@@ -152,9 +188,11 @@ public class MainActivity extends AppCompatActivity {
                             points.setText("" + score);
                             count++;
                         }
-                        else if(targetRight2 > imgRight && targetLeft2 < imgLeft && targetTop2 > imgTop
+                        else if(targetRight2 > imgRight && targetLeft2 < imgLeft && targetTop2 < imgTop
                                 && targetBottom2 > imgBottom && image3.getTag().equals("trash"))
                         {
+                            //debugchannel.setText("lmaoHA"+"target right: " + targetRight2 + "; target left: " + targetLeft2 + "; img right: " + imgRight + "; img left: " + imgLeft
+                                 //    + "target top: " + targetTop2 + "; target bottom: " + targetBottom2 + "; img top: " + imgTop + "; img bottom: " + imgBottom);
                             hitCheck();
                         }
                     }
